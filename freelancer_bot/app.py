435b9b3265ab50119fbcaaaced9dc9fce316f61f
pica105 <glebkulakov821@gmail.com>
2026-05-17 14:27:55 +0300

    quick fixes

--- freelancer_bot/app.py
+++ freelancer_bot/app.py
@@ -106,7 +106,7 @@ class LeadBot:
         self._kwork_handle = "@freelance_dev_work"
         self._openrouter_api_key = config.openrouter_api_key
         self._openrouter_model = config.openrouter_model
-        self._ai_basic_url = config.openrouter_ai_basic_url
+        self._ai_basic_url = config.ai_basic_url
         self._kwork_offered: set[str] = set()
 
     async def run(self) -> None:
